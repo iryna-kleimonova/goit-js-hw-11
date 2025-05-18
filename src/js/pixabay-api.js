3c06fcf2e9cf273d59c90b48288762a99f941834
semicvetik <semikirina@ukr.net>
2025-05-18 21:45:58 +0200

    add

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '50365477-8c0461eb0e95756bab65f9c1a';
+
+export async function getImagesByQuery(query) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
